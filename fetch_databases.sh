a951fd579cf5077d34a6d7599fb151232969ff84
Tom Morrison <tm22@sanger.ac.uk>
2026-06-13 14:55:27 +0100

    add fetch_databases.sh

---
+++ fetch_databases.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# sylph db
+wget http://faust.compbio.cs.cmu.edu/sylph-stuff/gtdb-r232-c1000-dbv1.syldb
+
+# checkm2 db
+wget https://zenodo.org/records/14897628/files/checkm2_database.tar.gz?download=1
+
+# bakta db
+wget https://zenodo.org/records/14916843/files/db-light.tar.xz?download=1
