3a77b88e1e0393dccd8d52765cb3f268e98b5ce8
Tom Morrison <tm22@sanger.ac.uk>
2026-06-14 14:23:59 +0100

    add fetch_test_data.sh

---
+++ fetch_test_data.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR120/ERR12024995/p78_d1_A9_R2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR120/ERR12024988/p50_d1_A8_R1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR120/ERR12024988/p50_d1_A8_R2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR120/ERR12024990/p80_d1_F5_R1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR120/ERR12024990/p80_d1_F5_R2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/run/ERR120/ERR12024995/p78_d1_A9_R1.fastq.gz
